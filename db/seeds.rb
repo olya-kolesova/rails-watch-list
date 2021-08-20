a89b162f8d663b51805532cd2955d00e4a4f8491
Olga Kolesova <olka.kolesova@gmail.com>
2021-08-20 11:15:23 +0100

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "open-uri"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,12 +6,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Movie.create(
+movie = Movie.create(
   title: "Wonder Woman 1984",
   overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s",
   poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg",
   rating: 6.9
 )
+movie.photo.attach
 Movie.create(
   title: "The Shawshank Redemption",
   overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison",
